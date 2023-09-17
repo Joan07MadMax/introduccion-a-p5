82ce5416ed2138fb9ceb71aa29b9d7dadb0a8f3e
Joan07MadMax <97638171+Joan07MadMax@users.noreply.github.com>
2023-09-17 17:33:48 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function setup() {
+    canvas = createCanvas(650, 500);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+    tint_color = "";
+}
+function draw() {
+    image(video, 0, 0, 650, 500);
+    tint(tint_color);
+}
+function take_snapshot() {
+    save("thiago.png");
+}
+function filter_tint() {
+    tint_color = document.getElementById("color").value;
+}
